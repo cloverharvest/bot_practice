f223231e8f7b6843f978cb4fe583a2c94760d969
cloverharvest <anna@schwabdev.com>
2016-05-12 11:27:53 -0700

    connected to websocket

--- play.rb
+++ play.rb
@@ -5,15 +5,12 @@ require 'faye/websocket'
 # require 'sinatra'
 
 
-#test connection with slack
-rc = HTTP.post("https://slack.com/api/auth.test", params: {token: ENV['SLACK_API_TOKEN']})
-
-#bot posts a message on slack
-rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+#try to get web socket connection
+rc = HTTP.post("https://slack.com/api/rtm.start", params: {
   token: ENV['SLACK_API_TOKEN'],
   channel: '#random',
-  text: 'Hello from the Schwabs of Benicia!',
-  as_user: true
 })
 
-puts JSON.pretty_generate(JSON.parse(rc.body))
+rc = JSON.parse(rc.body)
+
+puts rc['url']
