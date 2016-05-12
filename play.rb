c5a311a840e9780531a0e090278b91cb93cdf9dc
cloverharvest <anna@schwabdev.com>
2016-05-12 11:35:02 -0700

    Bot can listen to events

--- play.rb
+++ play.rb
@@ -5,12 +5,38 @@ require 'faye/websocket'
 # require 'sinatra'
 
 
-#try to get web socket connection
+# bot can actually listen to things, let's obtain a websocket url
+# starts a real-time messaging session
+# does an rtm.start call and we will get back a URL
+#
 rc = HTTP.post("https://slack.com/api/rtm.start", params: {
-  token: ENV['SLACK_API_TOKEN'],
-  channel: '#random',
+  token: ENV['SLACK_API_TOKEN']
 })
 
 rc = JSON.parse(rc.body)
 
-puts rc['url']
+url = rc['url']
+
+EM.run do
+  ws = Faye::WebSocket::Client.new(url)
+
+  ws.on :open do
+    p [:open]
+  end
+
+  ws.on :message do |event|
+    p [:message, JSON.parse(event.data)]
+    data = JSON.parse(event.data)
+    if data['text'] == 'hi'
+      ws.send({ type: 'message',
+        text: "Hello <%#{data['user']}> !",
+        channel: data['channel'] }.to_json)
+    end
+  end
+
+  ws.on :close do
+    p [:close, event.code, event.reason]
+    ws = nil
+    EM.stop
+  end
+end
